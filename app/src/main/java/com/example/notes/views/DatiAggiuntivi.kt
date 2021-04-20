b5d280e9f82db6829bcf1b30d0b774ff123880c6
llake_ethur <buia.matteo@outlook.com>
2021-04-20 15:34:13 +0200

    Progetto Modis Academy

--- app/src/main/java/com/example/notes/views/DatiAggiuntivi.kt
+++ app/src/main/java/com/example/notes/views/DatiAggiuntivi.kt
@@ -2,6 +2,7 @@ package com.example.notes.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notes.R
 
 class DatiAggiuntivi : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
