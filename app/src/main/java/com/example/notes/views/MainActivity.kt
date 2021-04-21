f81187534bd13b2337babad6619f5d60bedb02e2
federico-grava <federico.grava@gmail.com>
2021-04-21 17:05:19 +0200

    aggiunti testi

--- app/src/main/java/com/example/notes/views/MainActivity.kt
+++ app/src/main/java/com/example/notes/views/MainActivity.kt
@@ -11,6 +11,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
+
+
+        with(binding){
+            textEmail
+        }
+
         setContentView(binding.root)
     }
 }
