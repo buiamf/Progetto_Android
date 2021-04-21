4ec6298a68da6e35bc7b2e1e462c9c7a1e888d32
llake_ethur <buia.matteo@outlook.com>
2021-04-21 09:41:05 +0200

    Progetto Modis Academy

--- app/src/main/java/com/example/notes/views/MainActivity.kt
+++ app/src/main/java/com/example/notes/views/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.notes.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import com.example.notes.R
+import com.example.notes.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
